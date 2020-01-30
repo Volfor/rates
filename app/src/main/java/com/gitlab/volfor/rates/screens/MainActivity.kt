50b7014aa26af54880a4becd4232b05a529ffe46
Volfor <valentine.trifonov@gmail.com>
2020-01-30 21:08:31 +0200

    added simple list

--- app/src/main/java/com/gitlab/volfor/rates/screens/MainActivity.kt
+++ app/src/main/java/com/gitlab/volfor/rates/screens/MainActivity.kt
@@ -3,9 +3,11 @@ package com.gitlab.volfor.rates.screens
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.gitlab.volfor.rates.App
 import com.gitlab.volfor.rates.R
+import com.gitlab.volfor.rates.databinding.ActivityMainBinding
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -14,11 +16,15 @@ class MainActivity : AppCompatActivity() {
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
     private val vm by viewModels<MainViewModel> { viewModelFactory }
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         (application as App).appComponent.inject(this)
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.vm = vm
+        binding.lifecycleOwner = this
     }
 }
--- app/src/main/java/com/gitlab/volfor/rates/screens/MainViewModel.kt
+++ app/src/main/java/com/gitlab/volfor/rates/screens/MainViewModel.kt
@@ -1,12 +1,17 @@
 package com.gitlab.volfor.rates.screens
 
 import androidx.lifecycle.ViewModel
+import com.gitlab.volfor.rates.BR
+import com.gitlab.volfor.rates.R
 import com.gitlab.volfor.rates.repositories.RatesRepository
+import me.tatarka.bindingcollectionadapter2.ItemBinding
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(
     private val ratesRepository: RatesRepository
 ) : ViewModel() {
 
+    val items = listOf("EUR", "USD", "RUB")
+    val itemBinding = ItemBinding.of<String>(BR.item, R.layout.item_currency)
 
 }
