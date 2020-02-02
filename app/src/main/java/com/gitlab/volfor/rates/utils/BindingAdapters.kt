37b560e8e8e89dbb7aee68d50b5277171116e679
Volfor <valentine.trifonov@gmail.com>
2020-02-02 15:43:15 +0200

    added currencies icons

--- app/src/main/java/com/gitlab/volfor/rates/utils/BindingAdapters.kt
+++ app/src/main/java/com/gitlab/volfor/rates/utils/BindingAdapters.kt
@@ -1,7 +1,18 @@
 package com.gitlab.volfor.rates.utils
 
 import android.view.View
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
 import androidx.databinding.BindingConversion
 
 @BindingConversion
 fun convertBooleanToVisibility(isVisible: Boolean) = if (isVisible) View.VISIBLE else View.GONE
+
+@BindingAdapter("currencyFlag")
+fun setCurrencyFlagImage(v: ImageView, currencyCode: String) {
+    val resName = "ic_${currencyCode.toLowerCase()}"
+    val resId = v.resources.getIdentifier(resName, "drawable", v.context.packageName)
+    val drawable = if (resId != 0) ContextCompat.getDrawable(v.context, resId) else null
+    v.setImageDrawable(drawable)
+}
